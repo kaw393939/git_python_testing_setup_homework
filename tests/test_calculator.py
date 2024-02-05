1b2e1e6c660140e1f7f7b829fc2695e957b74061
kaw393939 <kwilliam@njit.edu>
2024-02-05 15:10:40 -0500

    fixed test coverage

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,11 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that addition function works '''    
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that addition function works '''    
+    assert Calculator.divide(2,2) == 1
